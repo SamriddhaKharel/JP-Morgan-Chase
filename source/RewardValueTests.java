9474bf12866786282da2a0ecc4cbc243631a8e26
samdm <samdm@DESKTOP-FBLLGMN>
2024-08-17 16:13:39 -0500

    testing

--- source/RewardValueTests.java
+++ source/RewardValueTests.java
@@ -8,23 +8,31 @@ public class RewardValueTests {
     void create_with_cash_value() {
         double cashValue = 100;
         var rewardValue = new RewardValue(cashValue);
-        assertEquals(cashValue, rewardValue.getCashValue());
+        assertEquals(cashValue, rewardValue.getCashValue(), 0.01);
+        assertEquals(cashValue / 0.0035, rewardValue.getMilesValue(), 0.01);
     }
 
     @Test
     void create_with_miles_value() {
-        int milesValue = 10000;
+        double milesValue = 10000;
         var rewardValue = new RewardValue(milesValue);
-        assertEquals(milesValue, rewardValue.getMilesValue());
+        assertEquals(milesValue, rewardValue.getMilesValue(), 0.01);
+        assertEquals(milesValue * 0.0035, rewardValue.getCashValue(), 0.01);
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 200;
+        var rewardValue = new RewardValue(cashValue);
+        double expectedMiles = cashValue / 0.0035;
+        assertEquals(expectedMiles, rewardValue.getMilesValue(), 0.01);
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        double milesValue = 5000;
+        var rewardValue = new RewardValue(milesValue);
+        double expectedCash = milesValue * 0.0035;
+        assertEquals(expectedCash, rewardValue.getCashValue(), 0.01);
     }
 }
--- source/RewardsConverter.java
+++ source/RewardsConverter.java
@@ -1,33 +1,69 @@
 import java.util.Scanner;
 
-public class RewardsConverter {
-    public static void main(String[] args) {
+public class RewardsConverter 
+{
+    public static void main(String[] args) 
+    {
+        // Creating a Scanner object to read input from the user.
         var scanner = new Scanner(System.in);
+
         System.out.println("Welcome to the Credit Card Rewards Converter!");
+
+        // Prompting the user to enter a cash value that they want to convert to airline miles.
         System.out.println("Please enter a cash value to convert to airline miles: ");
+        
+        // Reading the user's input as a string.
         var input_value = scanner.nextLine();
+
         double cashValue;
-        try {
+        try 
+        {
+            // converting the user's input from a string to a double.
             cashValue = Double.parseDouble(input_value);
-        } catch (NumberFormatException exception) {
+        } 
+        catch (NumberFormatException exception) 
+        {
+            // If the input cannot be converted to a double, print an error message and exit.
             System.out.println("Could not parse input value as a double, exiting");
-            return;
+            return;  // Exit the program early because of the error.
         }
-        System.out.println("converting $" + input_value + " to miles");
+
+        // Print a message indicating that the conversion is happening.
+        System.out.println("Converting $" + input_value + " to miles");
+        
+        // Create a RewardValue object using the cash value entered by the user.
         var rewardsValue = new RewardValue(cashValue);
+
+        // Print out the converted value in miles.
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
     }
 }
 
-class RewardValue {
+class RewardValue 
+{
     private double cashValue;
-    private static final double MILES_CONVERSION_RATE = 0.5; // Example conversion rate
+    private double milesValue;
+    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
+    // Constructor that accepts a cash value
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
+        this.milesValue = cashValue / MILES_TO_CASH_CONVERSION_RATE;
     }
 
+    // Constructor that accepts a miles value
+    public RewardValue(double milesValue, boolean isMiles) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    // Method to get the cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Method to get the miles value
     public double getMilesValue() {
-        return cashValue * MILES_CONVERSION_RATE;
+        return milesValue;
     }
 }
